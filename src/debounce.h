cbe155ac4bd3422bb9431fc24780d86a964fa2d3
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-03-17 09:12:48 +0100

    Update debounce.h

--- src/debounce.h
+++ src/debounce.h
@@ -11,11 +11,23 @@ class Debounce {
     bool lastReading;
 
   public:
+    /**
+     * Construct a new instance to debounce a button.
+     * 
+     * @param digitalReadFunction The function for reading the button
+     * @param debounceDelayMs The debounce delay in milliseconds
+     * @param millisFunction The millis function
+     */
     Debounce(
       bool (*digitalReadFunction)(),
       uint16_t debounceDelayMs = 10,
       unsigned long (*millisFunction)() = millis
     );
 
+    /**
+     * This function returns the current state of the button.
+     * 
+     * @return The current state of the button
+     */
     bool read();
 };
